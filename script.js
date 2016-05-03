5da895f2e4a283bdd1536d31bfcd499a5deccbb0
Sciensio <chris.colleran@gmail.com>
2016-05-03 11:21:34 -0600

    Added files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const _ = require('lodash');
+const Script = require('smooch-bot').Script;
+
+const scriptRules = require('./script.json');
+
+function wait(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+module.exports = new Script({
+    processing: {
+        //prompt: (bot) => bot.say('Beep boop...'),
+        receive: () => 'processing'
+    },
+
+    start: {
+        receive: (bot) => {
+            return bot.say('At any time you can use INFO to see your options')
+                .then(() => 'speak');
+        }
+    },
+
+    speak: {
+        receive: (bot, message) => {
+
+            let upperText = message.text.trim().toUpperCase();
+
+            function updateSilent() {
+                switch (upperText) {
+                    case "CONNECT ME":
+                        return bot.setProp("silent", true);
+                    case "DISCONNECT":
+                        return bot.setProp("silent", false);
+                    default:
+                        return Promise.resolve();
+                }
+            }
+
+            function getSilent() {
+                return bot.getProp("silent");
+            }
+
+            function processMessage(isSilent) {
+                if (isSilent) {
+                    return Promise.resolve("speak");
+                }
+
+                if (!_.has(scriptRules, upperText)) {
+                    //return bot.say(`So, I'm good at structured conversations but stickers, emoji and sentences still confuse me. I'm still learning - thanks for understanding!`).then(() => 'speak');
+                    return bot.say(bot.name).then(() => 'speak');
+                }
+
+                var response = scriptRules[upperText];
+                var lines = response.split('\n');
+
+                var p = Promise.resolve();
+                _.each(lines, function(line) {
+                    line = line.trim();
+                    p = p.then(function() {
+                        console.log(line);
+                        return wait(50).then(function() {
+                            return bot.say(line);//line
+                        });
+                    });
+                });
+
+                return p.then(() => 'speak');
+            }
+
+            return updateSilent()
+                .then(getSilent)
+                .then(processMessage);
+        }
+    }
+});
